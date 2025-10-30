a2a7d6a8d4c04de4d5bcd5887ecbd058f43dc882
akmaalsyh <taufansyah50@gmail.com>
2025-10-30 08:51:18 +0700

    menambahkan variabel untuk mengingat nilai yang sudah dimasukkan

---
+++ app/src/main/java/com/example/questuserinput_236/TeksEdit.kt
@@ -0,0 +1,13 @@
+package com.example.questuserinput_236
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+fun FormDataDiri(modifier: Modifier)
+{
+    // Variabel-variabel untuk mengingat nilai masukan dari
+    var textNama by remember { mutableStateOf(value = "") }
+    var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
+}
