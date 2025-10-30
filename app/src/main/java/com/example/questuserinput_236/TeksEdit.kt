b1771d5b9acac55b9f7ea394c9412173c36d202e
akmaalsyh <taufansyah50@gmail.com>
2025-10-30 08:53:36 +0700

    menambahkan variabel untuk menyimpan data atau input data

--- app/src/main/java/com/example/questuserinput_236/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_236/TeksEdit.kt
@@ -10,4 +10,9 @@ fun FormDataDiri(modifier: Modifier)
     var textNama by remember { mutableStateOf(value = "") }
     var textAlamat by remember { mutableStateOf(value = "") }
     var textJK by remember { mutableStateOf(value = "") }
+
+    // variabel-variabel untuk menyimpan data yang diperoleh dari
+    var nama by remember { mutableStateOf(value = "") }
+    var alamat by remember { mutableStateOf(value = "") }
+    var jenis by remember { mutableStateOf(value = "") }
 }
