99a9783f32dc9ad9dfcf319d62e70b68f467f168
akmaalsyh <taufansyah50@gmail.com>
2025-11-01 14:44:17 +0700

    menambahkan card sebagai background

--- app/src/main/java/com/example/questuserinput_236/TeksEdit.kt
+++ app/src/main/java/com/example/questuserinput_236/TeksEdit.kt
@@ -4,12 +4,14 @@ package com.example.questuserinput_236
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.HorizontalDivider
@@ -25,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -41,20 +44,18 @@ fun FormDataDiri(modifier: Modifier)
     val gender:List<String> = listOf("Laki-laki", "Perempuan")
     val status:List<String> = listOf("Janda", "Lajang", "Duda")
 
-    Column (modifier = Modifier.padding(top = 50.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally)
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                bottom = dimensionResource(R.dimen.padding_medium),
+                top = dimensionResource(R.dimen.padding_medium)),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFFF3E5F5))
+    )
     {
-        OutlinedTextField(
-            value = textNama,
-            singleLine = true,
-            shape = MaterialTheme.shapes.large,
-            modifier = modifier.width(width = 250.dp),
-            label = { Text(text = "Nama Lengkap") },
-            onValueChange = {
-                textNama = it
+
             }
-        )
+
         Row {
             gender.forEach { item ->
                 Row(
